c98ff632616a709cbeb9dd8b0fddeadfc4cb435f
[lince_academye] <[julianokeil@gmail.com]>
2023-06-25 18:34:59 -0300

    aula sobre operadors

---
+++ curso06.dart
@@ -0,0 +1,81 @@
+// void main() {
+//   dynamic produto = Produto();
+//   print((produto as item).nome);
+// }
+
+// class Produto {
+//   final nome = 'nome';
+// }
+
+// class item extends Produto {
+//   final nome = 'teclado';
+// }
+
+// void main() {
+//   // =
+//   //+-
+//   //-=
+//   //*=
+//   //
+
+//   var numero = getNumero();
+
+//   numero ??= 50;
+//   print(numero);
+
+// int? getNumero() => 10;
+
+// void main() {
+//   bool camisetaBranca = true;
+//   bool blusaAzul = false;
+
+//   if (camisetaBranca || blusaAzul) { // ou usar && mas os dois tem que ser verdadeiro
+//     print('Pode entrar na festa');
+//   } else {
+//     print('Não pode entrar');
+//   }
+// }
+
+//void main() {
+// condition ? expr1 : expre2 /chama uma das duas
+// expr1 ?? expr2 /caso for nulo
+
+// bool conteudo = false;//  muda de lado caso seja  true
+
+// final resultado = conteudo ? 'publico' : 'privado';
+
+// print(resultado);
+
+//   String? conteudo = getConteudo();
+//   final resultado = conteudo ?? 'estudar mais dart';
+
+//   print(resultado);
+// }
+
+// String? getConteudo() => null;
+void main() {
+  // final container01 = Container();
+  // container01.cor = "black";
+  // container01.altura = 2.0;
+  // container01.largura = 90;
+
+  // final container_2 = Container()
+  //   ..cor = 'black'
+  //   ..altura = 2.0
+  //   ..largura = 90;
+
+//   final container = getcontainer()
+//     ?..cor = 'black'
+//     ..altura = 2.0
+//     ..largura = 90;
+
+//   Container? getcontainer() {
+//     return Container();
+//   }
+// }
+
+// class Container {
+//   late String cor;
+//   late double altura;
+//   late double largura;
+}
